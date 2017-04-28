3a9b9f26c4a90c74aa8e30cb0f5fe58f4475e0c8
Dayan Perez <dayan.perez9012@gmail.com>
2017-04-28 13:42:47 -0400

    Update section map in home page

--- resources/views/frontend/views/home.blade.php
+++ resources/views/frontend/views/home.blade.php
@@ -15,67 +15,70 @@
                      data-duration="2000"
                      data-delay="700"
                      data-ease="easeOutExpo"
-                >Welcome</div>
+                >Welcome
+                </div>
 
                 <div class="ms-layer ms-promo-info color-light" style="top:180px"
                      data-effect="bottom(40)"
                      data-duration="2000"
                      data-delay="1000"
                      data-ease="easeOutExpo"
-                >SKY TECHNICS AVIATION REPAIRS</div>
+                >SKY TECHNICS AVIATION REPAIRS
+                </div>
 
                 <div class="ms-layer ms-promo-sub color-light" style="top:280px"
                      data-effect="bottom(40)"
                      data-duration="2000"
                      data-delay="1300"
                      data-ease="easeOutExpo"
-                >SYNONYM OF PROFESSIONALISM, EFFICIENCY <br> AND FIRST CLASS SERVICE</div>
+                >SYNONYM OF PROFESSIONALISM, EFFICIENCY <br> AND FIRST CLASS SERVICE
+                </div>
             </div>
 
             <div class="ms-slide" style="z-index: 13">
                 <img src="frontend/img/sliders/Avion2.png" data-src="frontend/img/sliders/Avion2.png" alt="">
 
                 {{--<img class="ms-layer" src="frontend/img/mockup/hand-black-iphone-l.png" data-src="frontend/img/mockup/hand-black-iphone-l.png" alt=""--}}
-                     {{--style="bottom:-10px; left:15px; width:400px; height: auto;"--}}
-                     {{--data-effect="bottom(100)"--}}
-                     {{--data-duration="2000"--}}
-                     {{--data-ease="easeOutExpo"--}}
-                     {{--data-type="image"--}}
+                {{--style="bottom:-10px; left:15px; width:400px; height: auto;"--}}
+                {{--data-effect="bottom(100)"--}}
+                {{--data-duration="2000"--}}
+                {{--data-ease="easeOutExpo"--}}
+                {{--data-type="image"--}}
                 {{--/>--}}
 
                 {{--<h3 class="ms-layer ms-promo-info-in color-light"  style="left:450px; top:170px;"--}}
-                    {{--data-effect="right(40)"--}}
-                    {{--data-duration="2300"--}}
-                    {{--data-delay="1300"--}}
-                    {{--data-ease="easeOutExpo"--}}
+                {{--data-effect="right(40)"--}}
+                {{--data-duration="2300"--}}
+                {{--data-delay="1300"--}}
+                {{--data-ease="easeOutExpo"--}}
                 {{-->MOST</h3>--}}
 
                 {{--<h3 class="ms-layer ms-promo-info-in color-darker"  style="left:450px; top:230px"--}}
-                    {{--data-effect="left(40)"--}}
-                    {{--data-duration="2300"--}}
-                    {{--data-delay="1400"--}}
-                    {{--data-ease="easeOutBack"--}}
+                {{--data-effect="left(40)"--}}
+                {{--data-duration="2300"--}}
+                {{--data-delay="1400"--}}
+                {{--data-ease="easeOutBack"--}}
                 {{-->INCREDIBLE</h3>--}}
 
                 {{--<h3 class="ms-layer ms-promo-info-in color-darker"  style="left:450px; top:290px"--}}
-                    {{--data-effect="left(40)"--}}
-                    {{--data-duration="2300"--}}
-                    {{--data-delay="1400"--}}
-                    {{--data-ease="easeOutBack"--}}
+                {{--data-effect="left(40)"--}}
+                {{--data-duration="2300"--}}
+                {{--data-delay="1400"--}}
+                {{--data-ease="easeOutBack"--}}
                 {{-->NEW <span class="color-light">FEATURES</span></h3>--}}
 
                 {{--<a class="ms-layer btn-u" style="left:450px; top:370px" href="#"--}}
-                   {{--data-effect="bottom(40)"--}}
-                   {{--data-duration="2000"--}}
-                   {{--data-delay="1300"--}}
-                   {{--data-ease="easeOutExpo"--}}
+                {{--data-effect="bottom(40)"--}}
+                {{--data-duration="2000"--}}
+                {{--data-delay="1300"--}}
+                {{--data-ease="easeOutExpo"--}}
                 {{-->LEARN MORE</a>--}}
 
                 {{--<a class="ms-layer btn-u btn-u-dark" style="left:580px; top:370px" href="#"--}}
-                   {{--data-effect="bottom(40)"--}}
-                   {{--data-duration="2000"--}}
-                   {{--data-delay="1300"--}}
-                   {{--data-ease="easeOutExpo"--}}
+                {{--data-effect="bottom(40)"--}}
+                {{--data-duration="2000"--}}
+                {{--data-delay="1300"--}}
+                {{--data-ease="easeOutExpo"--}}
                 {{-->DOWNLOAD NOW</a>--}}
             </div>
 
@@ -83,41 +86,41 @@
                 <img src="frontend/img/sliders/Avion3.png" data-src="frontend/img/sliders/Avion3.png" alt="">
 
                 {{--<div class="ms-layer video-box" style="bottom:125px; right:15px; width:650px; height:370px;"--}}
-                     {{--data-type="video"--}}
-                     {{--data-effect="rotate3dright(0,30,0,100,r)"--}}
-                     {{--data-duration="1500"--}}
-                     {{--data-ease="easeOutQuad"--}}
+                {{--data-type="video"--}}
+                {{--data-effect="rotate3dright(0,30,0,100,r)"--}}
+                {{--data-duration="1500"--}}
+                {{--data-ease="easeOutQuad"--}}
                 {{-->--}}
-                    {{--<img class="ms-img-bordered" src="frontend/img/main/img20.jpg" data-src="frontend/img/main/img20.jpg" alt="">--}}
-                    {{--<iframe src="http://player.vimeo.com/video/70528799" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen> </iframe>--}}
+                {{--<img class="ms-img-bordered" src="frontend/img/main/img20.jpg" data-src="frontend/img/main/img20.jpg" alt="">--}}
+                {{--<iframe src="http://player.vimeo.com/video/70528799" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen> </iframe>--}}
                 {{--</div>--}}
 
                 {{--<h3 class="ms-layer ms-promo-info color-light"  style="left:10px; top:170px"--}}
-                    {{--data-effect="bottom(20)"--}}
-                    {{--data-duration="2300"--}}
-                    {{--data-delay="2300"--}}
-                    {{--data-ease="easeOutExpo"--}}
+                {{--data-effect="bottom(20)"--}}
+                {{--data-duration="2300"--}}
+                {{--data-delay="2300"--}}
+                {{--data-ease="easeOutExpo"--}}
                 {{-->AMAZING</h3>--}}
 
                 {{--<h3 class="ms-layer ms-promo-info-in color-light"  style="left:10px; top:245px"--}}
-                    {{--data-effect="left(100)"--}}
-                    {{--data-duration="3300"--}}
-                    {{--data-delay="1900"--}}
-                    {{--data-ease="easeOutExpo"--}}
+                {{--data-effect="left(100)"--}}
+                {{--data-duration="3300"--}}
+                {{--data-delay="1900"--}}
+                {{--data-ease="easeOutExpo"--}}
                 {{--><span class="color-green">FEATURES</span></h3>--}}
 
                 {{--<h3 class="ms-layer normal-title color-light"  style="left:10px; top:312px"--}}
-                    {{--data-effect="bottom(20)"--}}
-                    {{--data-duration="2300"--}}
-                    {{--data-delay="2000"--}}
-                    {{--data-ease="easeOutExpo"--}}
+                {{--data-effect="bottom(20)"--}}
+                {{--data-duration="2300"--}}
+                {{--data-delay="2000"--}}
+                {{--data-ease="easeOutExpo"--}}
                 {{-->UNIFY BOOTSTRAP TEMPLATE</h3>--}}
 
                 {{--<p class="ms-layer ms-promo-sub ms-promo-sub-in color-light"  style="left:10px; top:360px"--}}
-                   {{--data-effect="right(40)"--}}
-                   {{--data-duration="2300"--}}
-                   {{--data-delay="2300"--}}
-                   {{--data-ease="easeOutExpo"--}}
+                {{--data-effect="right(40)"--}}
+                {{--data-duration="2300"--}}
+                {{--data-delay="2300"--}}
+                {{--data-ease="easeOutExpo"--}}
                 {{-->YOUTUBE, VIMEO AND CUSTOM <br> IFRAME SUPPORTED</p>--}}
             </div>
         </div>
@@ -130,8 +133,14 @@
         <div class="row">
             <div class="col-md-6 wow slideInLeft">
                 <h2 class="title-v2">WE ARE STAR</h2>
-                <p style="font-size: 16px;" class="text-justify">We put pride in everything we do here at STAR. When we put our name on your aircraft parts, we are not just trying to do a job, its our duty to make sure it is done with high quality.We are a new company but with many years of expertise and experience. We meet compliance and high standards and provide professional work from technicians that have been in the field for many years (15 +).</p>
-                <p style="font-size: 16px;" class="text-justify">That is why we guarantee the best service around.Our company holds itself to excellence and knows the importance of getting your aircraft up in the air.</p><br>
+                <p style="font-size: 16px;" class="text-justify">We put pride in everything we do here at STAR. When we
+                    put our name on your aircraft parts, we are not just trying to do a job, its our duty to make sure
+                    it is done with high quality.We are a new company but with many years of expertise and experience.
+                    We meet compliance and high standards and provide professional work from technicians that have been
+                    in the field for many years (15 +).</p>
+                <p style="font-size: 16px;" class="text-justify">That is why we guarantee the best service around.Our
+                    company holds itself to excellence and knows the importance of getting your aircraft up in the
+                    air.</p><br>
                 <a href="#" class="btn-u btn-brd btn-brd-hover btn-u-dark">Read More</a>
                 <a href="#" class="btn-u">View Our Work</a>
             </div>
@@ -146,7 +155,8 @@
     <div class="parallax-quote parallaxBg wow slideInUp">
         <div class="container">
             <div class="parallax-quote-in">
-                <p style="font-size: 20px;">Sky Technics Aviation Repairs synonym of<span class="color-green"> PROFESSIONALISM, EFFICIENCY AND FIRST CLASS SERVICE.</span> Those are our main qualities and principles of what we stand for.</p>
+                <p style="font-size: 20px;">Sky Technics Aviation Repairs synonym of<span class="color-green"> PROFESSIONALISM, EFFICIENCY AND FIRST CLASS SERVICE.</span>
+                    Those are our main qualities and principles of what we stand for.</p>
             </div>
         </div>
     </div>
@@ -164,7 +174,10 @@
                 <div class="service-block-v7 wow bounceInLeft">
                     <i class="icon-settings"></i>
                     <h3 class="title-v3-bg text-uppercase">MRO Solution</h3>
-                    <p class="text-justify">STAR state-of-the-art facilities and highly trained professionals allow us to provide quality in-house maintenance and repair services. We can test, repair, and overhaul a range of Avionic instruments, Electro Mechanical Components, Pneumatic components, Hydraulic Components and Fuel Components.</p>
+                    <p class="text-justify">STAR state-of-the-art facilities and highly trained professionals allow us
+                        to provide quality in-house maintenance and repair services. We can test, repair, and overhaul a
+                        range of Avionic instruments, Electro Mechanical Components, Pneumatic components, Hydraulic
+                        Components and Fuel Components.</p>
                     <br>
                     <a class="text-uppercase" href="#">Read More</a>
                 </div>
@@ -173,7 +186,10 @@
                 <div class="service-block-v7 wow bounceInUp">
                     <i class="icon-layers"></i>
                     <h3 class="title-v3-bg text-uppercase">Trading</h3>
-                    <p class="text-justify">We maintain and operate a large rotable spare parts pool in order to better serve our customers. With a range of aircraft components on-hand, we can accelerate our repair services through sales and exchanges as per the needs of each repair situation. By keeping an eye on our clients' present and feature fleet requirements.</p>
+                    <p class="text-justify">We maintain and operate a large rotable spare parts pool in order to better
+                        serve our customers. With a range of aircraft components on-hand, we can accelerate our repair
+                        services through sales and exchanges as per the needs of each repair situation. By keeping an
+                        eye on our clients' present and feature fleet requirements.</p>
                     <a class="text-uppercase" href="#">Read More</a>
                 </div>
             </div>
@@ -181,7 +197,10 @@
                 <div class="service-block-v7 wow bounceInRight">
                     <i class="icon-plane"></i>
                     <h3 class="title-v3-bg text-uppercase">Military Solutions</h3>
-                    <p class="text-justify">STAR is proud to assist military aircraft fleets with repairs, maintenance, logistic support, and component supply solutions. Our professionals have the experience and knowledge of Air Force Technical Orders and manuals to effectively and quickly repair military avionics instruments and components and provide other.</p>
+                    <p class="text-justify">STAR is proud to assist military aircraft fleets with repairs, maintenance,
+                        logistic support, and component supply solutions. Our professionals have the experience and
+                        knowledge of Air Force Technical Orders and manuals to effectively and quickly repair military
+                        avionics instruments and components and provide other.</p>
                     <a class="text-uppercase" href="#">Read More</a>
                 </div>
             </div>
@@ -270,41 +289,6 @@
         </div><!--/end row-->
     </div>
     <!--=== End Team v4 ===-->
-
-    <!--=== Owl Clients v1 ===-->
-    <div class="container content-sm">
-        <div class="owl-clients-v1">
-            <div class="item">
-                <img src="frontend/img/clients4/1.png" alt="">
-            </div>
-            <div class="item">
-                <img src="frontend/img/clients4/2.png" alt="">
-            </div>
-            <div class="item">
-                <img src="frontend/img/clients4/3.png" alt="">
-            </div>
-            <div class="item">
-                <img src="frontend/img/clients4/4.png" alt="">
-            </div>
-            <div class="item">
-                <img src="frontend/img/clients4/5.png" alt="">
-            </div>
-            <div class="item">
-                <img src="frontend/img/clients4/6.png" alt="">
-            </div>
-            <div class="item">
-                <img src="frontend/img/clients4/7.png" alt="">
-            </div>
-            <div class="item">
-                <img src="frontend/img/clients4/8.png" alt="">
-            </div>
-            <div class="item">
-                <img src="frontend/img/clients4/9.png" alt="">
-            </div>
-        </div>
-    </div>
-    <!--=== End Owl Clients v1 ===-->
-
     <!--=== Map ===-->
     <div class="container-fluid">
         <div class="headline-center margin-bottom-60">
@@ -312,7 +296,7 @@
         </div>
         <div class="row">
             <div class="col-xs-12 no-padding" style="margin-bottom: -50px;">
-                <iframe scrolling="no" style="border: 0px none; border-color:#fff; width:100%;" height="350" src = "https://maps.google.com/maps?q=25.834936,-80.314053&hl=es;z=8&amp;output=embed"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1795.5234662284129!2d-80.31505614668801!3d25.834998763286904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDUwJzA1LjgiTiA4MMKwMTgnNTAuNiJX!5e0!3m2!1ses!2ses!4v1493399114818" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
         </div>
 
@@ -321,20 +305,14 @@
 @stop
 
 @push('scripts')
-    <script type="text/javascript">
-        jQuery(document).ready(function() {
-            App.init();
-            App.initCounter();
-            App.initParallaxBg();
-            FancyBox.initFancybox();
-            MSfullWidth.initMSfullWidth();
-            OwlCarousel.initOwlCarousel();
-        });
-
-        $('.map-container').click(function(){
-            $(this).find('iframe').addClass('clicked')
-        }).mouseleave(function(){
-            $(this).find('iframe').removeClass('clicked')
-        });
-    </script>
+<script type="text/javascript">
+    jQuery(document).ready(function () {
+        App.init();
+        App.initCounter();
+        App.initParallaxBg();
+        FancyBox.initFancybox();
+        MSfullWidth.initMSfullWidth();
+        OwlCarousel.initOwlCarousel();
+    });
+</script>
 @endpush
