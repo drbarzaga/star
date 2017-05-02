55034fb8355010f8d134ec3f06015dee1bf075bc
Dayan Perez <dayan.perez9012@gmail.com>
2017-05-02 10:24:57 -0400

    Update contact page and home page

--- resources/views/frontend/views/contact_us.blade.php
+++ resources/views/frontend/views/contact_us.blade.php
@@ -100,8 +100,17 @@
             <h2>LOCATION</h2>
         </div>
         <div class="row">
-            <div class="col-xs-12 no-padding" style="margin-bottom: -50px;">
+            <div id="mapa" class="col-xs-12 no-padding" style="margin-bottom: -50px;">
+                <div class="capa-anti-eventos" onclick="style.display='none'"></div>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1795.5234662284129!2d-80.31505614668801!3d25.834998763286904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDUwJzA1LjgiTiA4MMKwMTgnNTAuNiJX!5e0!3m2!1ses!2ses!4v1493399114818" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <style>
+                    #mapa .capa-anti-eventos {
+                        width: 600px;
+                        height: 450px;
+                        position: absolute;
+                        cursor: pointer;
+                    }
+                </style>
             </div>
         </div>
 
